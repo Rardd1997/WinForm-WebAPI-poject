1e3a5029e2af82523b6358d8ed38309911e3873b
artemxl <artemxl@gmail.com>
2017-01-31 17:02:16 +0200

    added post\put

--- Server/Server.API/Controllers/UserProfilesController.cs
+++ Server/Server.API/Controllers/UserProfilesController.cs
@@ -7,23 +7,53 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Server.Domain.Abstract;
 using Server.Domain.Entities;
 
 namespace Server.API.Controllers
 {
     public class UserProfilesController : ApiController
     {
+        private IDbRepository repository;
+
+        public UserProfilesController(IDbRepository dbRepository)
+        {
+            repository = dbRepository;
+        }
+
         [HttpGet]
         [ActionName("GetProfileByID")]
         public IEnumerable<Profile> GetProfile(int? id = null)
         {
             try
             {
-                return DbRepository.Repository.Profiles.Where(p => id == null || p.ProfileID == id);
+                return repository.Profiles.Where(p => id == null || p.ProfileID == id);
             }
             catch (Exception exc)
             {
                 return null;
+            }
+        }
+
+        [HttpPost]
+        [ActionName("PostProfile")]
+        public void PostProfile([FromBody]Profile profile)
+        {
+            repository.Profiles.Concat(new [] {profile});
+            repository.SaveChanges();
+        }
+
+        [HttpPut]
+        [ActionName("PutProfile")]
+        public void PutProfile(int id, [FromBody] Profile profile)
+        {
+            if (profile == null) return;
+
+            if (id == profile.ProfileID)
+            {
+                repository.Profiles.Where(p => p.ProfileID != id);
+                repository.Profiles.Concat(new[] {profile});
+                repository.SaveChanges();
             }
         }
    
---
+++ Server/Server.API/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using Server.Domain.Abstract;
+using Server.Domain.Concrete;
+
+namespace Server.API.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            ninjectKernel.Bind<IDbRepository>().To<AdoNetDbRepository>();
+        }
+    }
+}
